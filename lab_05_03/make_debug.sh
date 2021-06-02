1c1476f170a3b384b8cd1f066ef4b071ed3e9f74
Dinh Viet Anh <dv20iu54@student.bmstu.ru>
2021-06-02 20:05:18 +0300

    asd

--- lab_05_03/make_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc main.c -Wvla -Werror -Wpedantic -Wextra -Wvla -ggdb --coverage -c
-gcc process.c -Wvla -Werror -Wpedantic -Wextra -Wvla -ggdb --coverage -c
-gcc -o app.exe main.c process.o --coverage
--- lab_05_03/make_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc main.c -Wvla -Werror -Wpedantic -Wextra -Wvla -c
-gcc process.c -Wvla -Werror -Wpedantic -Wextra -Wvla -c
-gcc -o app.exe main.c process.o
